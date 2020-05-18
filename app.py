64a2f81e692af22562f2a4e8a8f5a013930c554a
André <andreamorimcouto22@gmail.com>
2020-05-18 22:32:56 +0100

    initial setup

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import uuid
+import time,datetime
+import flask
+
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template("home.html")
