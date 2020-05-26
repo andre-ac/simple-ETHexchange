9155314823810644bbedeec98657c366976f205e
André <andreamorimcouto22@gmail.com>
2020-05-26 23:35:51 +0100

    optimization of userdata api

--- static/scripts.js
+++ static/scripts.js
@@ -7,18 +7,28 @@ window.onload = function(){
 
 
 function get_userdata(){
-  $.get("/api/userinfo",function(data,status){
-    user_navbar_eth_balance.innerHTML = data.eth_balance;
-    user_navbar_usd_balance.innerHTML = data.usd_balance +" and time is " + data.time;
-    console.log(data)
-  });
+$.get("/api/userinfo",function(data,status){
+      user_navbar_eth_balance.innerHTML = data.eth_balance;
+      user_navbar_usd_balance.innerHTML = data.usd_balance +" and time is " + data.time;
+      console.log("get_userdata : " + status)
+      return status.status;
+    });
 };
 
 $(document).ready(function(){
   $("#alert_error").fadeToggle(3000);
 });
 
-
 $(document).ready(function(){
-  setInterval(function(){get_userdata()}, 500);
+
+  //makes sure get_userdata is only being requested when user is logged in (the balances in the nav is being shown)
+  if($("#user_navbar_eth_balance").is(":visible")){
+  setInterval(function(){
+    get_userdata()
+  }, 1000);}
+
+  else {
+    console.log("not logged in")
+  }
+
 });
