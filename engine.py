8a31e70f6ec98ecacb6ca667d0eff3111e75afdd
Andre-ac <andreamorimcouto22@gmail.com>
2020-06-09 23:46:18 +0100

    orderbook syncing improved

--- engine.py
+++ engine.py
@@ -53,42 +53,71 @@ def del_order_orderbook(order_id):
 
 
 def orderbook_sync():
-    """ Syncs both orderbook DB with all user's open orders"""
+    """ Syncs both orderbook DB with all user's open orders.
+        This uses a considerable amount of resources use wisely"""
 
     open_orders = db.execute("SELECT * FROM open_orders")
     hidden_orderbook = db.execute("SELECT * FROM hidden_orderbook")
     
-    #db.execute("DELETE FROM orderbook") 
-
+    db.execute("DELETE FROM orderbook") 
     #pass to orderbook
 
-    list_order_ids = []
-    list_prices = []
+    list_order_ids_on_hidden = []
+    list_order_ids_on_openorder = []
 
+    # get order ids available in the hidden orderbook before sync
     for openorders in hidden_orderbook:
-      list_order_ids.append(openorders["order_id"])
+      list_order_ids_on_hidden.append(openorders["order_id"])
 
+
+    # check if all open orders and if the quantities left are correct in the hidden orderbook 
     for order in open_orders:
       orderid = order["order_id"]
+      # get order ids available in the hidden orderbook before sync
+      list_order_ids_on_openorder.append(orderid)
+      quantity_left = order["quantity"]-order["filled"]
+      hidden_order = db.execute("SELECT * FROM hidden_orderbook WHERE order_id = :orderid", orderid= orderid)[0]
 
-      if order["order_id"] in list_order_ids:
+      if orderid in list_order_ids_on_hidden:
         print(f"{orderid} already there")
 
       else:
         print(f"{orderid} not here, adding it")
         db.execute("INSERT INTO hidden_orderbook (pair,price,quantity_left,order_id,timeplaced,type, user_id) VALUES(?,?,?,?,?,?,?)", order["pair"], order["price"], order["quantity"]-order["filled"], order["order_id"],order["time"],order["type"], order["user_id"])
     
-    for openorders in hidden_orderbook:
-      list_order_ids.append(openorders["order_id"])
-
-      if openorders["price"] in list_prices:
-        pass
+      if quantity_left != hidden_order["quantity_left"]:
+        db.execute("UPDATE hidden_orderbook SET quantity = :quantity", quantity = quantity_left)
+        print(f"{orderid} had wrong quantity in hidden, updated")
       else:
-        list_prices.append(openorders["price"])
+        print(f"{orderid} quantity was correct")
+      
 
-    print(list_prices)
+
+    for order in hidden_orderbook:
+      orderid = order["order_id"]
+
+      if orderid in list_order_ids_on_openorder:
+        print(f"{orderid} is in hidden and openorder, good")
+      else:
+        print(f"{orderid} was in hidden but not in open, deleted from hidden")
+        db.execute("DELETE FROM hidden_orderbook WHERE order_id = :orderid", orderid = orderid)
+
+    list_prices_already_on_orderbook = []
+    for order in open_orders:
+      
+      if order["price"] in list_prices_already_on_orderbook:
+        quantity_left = order["quantity"]-order["filled"]
+        query = db.execute("SELECT * FROM orderbook WHERE price = :price", price=order["price"])[0]
+        db.execute("UPDATE orderbook SET quantity= :quantity WHERE price=:price AND type = :type AND pair = :pair", quantity = query["quantity"]+quantity_left, price = order["price"], type = order["type"], pair= order["pair"])
+
+      else:
+        db.execute("INSERT INTO orderbook (pair,price,quantity,type) VALUES(?,?,?,?)", order["type"], order["price"], order["quantity"], order["type"])
+        list_prices_already_on_orderbook.append(order["price"])
+        print(str(order["price"]) + " added to orderbook")
+
 
 
 if __name__ == "__main__":
+    start_time = time.time()
     orderbook_sync()
-    print("Orderbook Synced")
+    print("Orderbook Synced in " + str((time.time() - start_time)*100)+ " ms")
